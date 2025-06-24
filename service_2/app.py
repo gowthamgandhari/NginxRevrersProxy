bc0f2dbb788ec92c64a2f0c0d210999ef55e27cc
gowthamgandhari <gowthamgandhari88@gmail.com>
2025-06-24 08:05:01 +0530

    / access for app.py

--- service_2/app.py
+++ service_2/app.py
@@ -10,11 +10,9 @@ def index():
 def ping():
     return jsonify(status="ok", service="2")
 
-
 @app.route("/hello")
 def hello():
     return jsonify(message="Hello from Service 2")
-
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8002)
